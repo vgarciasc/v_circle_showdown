5f1ae6e08c3d8b3a21be14b133e0a6798da4455d
littlevini\little vini <vingarciasc@gmail.com>
2016-12-12 15:37:50 -0200

    v0.3.2
    - Versão instável, backup

---
+++ Assets/Scripts/Title Screen/TitleScreenManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleScreenManager : MonoBehaviour {
+	public Image blackscreen;
+	public Animator startAnimator;
+	
+	void Update () {
+		if (Input.GetButtonDown("Submit")) {
+			callNextScene_();
+			//startAnimator.SetTrigger("start");
+		}
+	}
+
+	public void callNextScene_() { StartCoroutine(callNextScene()); }
+	IEnumerator callNextScene() {
+		blackscreen.enabled = true;
+		HushPuppy.fadeIn(blackscreen.gameObject, 2f);
+		yield return new WaitForSeconds(2.5f);
+
+		SceneManager.LoadScene("PlayerSelect");
+	}
+}
